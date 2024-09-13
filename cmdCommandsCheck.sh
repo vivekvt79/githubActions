d2c374d50802a0b1299576496c6d606b8f78b776
Vivek V T <46318848+vivekvt79@users.noreply.github.com>
2024-09-13 15:51:07 +0530

    Create cmdCommandsCheck.sh

---
+++ cmdCommandsCheck.sh
@@ -0,0 +1,9 @@
+echo What is in this directory
+ls -a
+echo
+echo Is Java installed?
+java -version
+echo Where is Selenium Jars?
+echo $SELENIUM_JAR_PATH
+echo What env variables are available?
+env
